43e52ac8f42d018e4d14f22aae474581dc8046d5
danc2015 <dconns11@Daniels-MacBook-Pro-4.local>
2016-08-10 01:09:10 -0400

    initial code upload to repo

---
+++ js/Controller.js
@@ -0,0 +1,9 @@
+ListBuilderApp.controller('mainCtrl', function($scope) {
+    
+//    .filter('listBuilderController',[
+//        function () {
+//            'use strict';
+//            return function () {
+//                /*your code */
+//            };
+         });
