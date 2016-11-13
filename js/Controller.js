a979df1926d9f245082642112ba52df28e5d08b1
danc2015 <dconns11@Daniels-MacBook-Pro-4.local>
2016-11-13 01:38:08 -0500

    moved angular code to new files after renaming directive

--- js/Controller.js
+++ js/Controller.js
@@ -1,9 +1,54 @@
-ListBuilderApp.controller('mainCtrl', function($scope) {
-    
-//    .filter('listBuilderController',[
-//        function () {
-//            'use strict';
-//            return function () {
-//                /*your code */
-//            };
-         });
+app.controller('mainCtrl', ['$scope', function($scope){
+    $scope.initialEntry = "";
+    $scope.finalList = [];
+    var basicListItem = {
+        listIndex: 0,
+        name: "",
+        completed: false
+    };
+
+    $scope.Init = function(){
+        var firstListItem = angular.copy(basicListItem);
+        $scope.finalList.push(firstListItem);
+    }
+    $scope.FormList = function(){   
+        ShowHide();
+        var initList = $scope.initialEntry;
+        //clear finalList to refresh upon resubmit
+        $scope.finalList = [];
+
+        var array = initList.split(",");
+        if(array.length > 0){
+            //insert into final array
+            for(var i = 0, len = array.length;i < len; i++){                    
+                $scope.CheckAndInsertItemFromBulkListEntry(array[i], i);
+            }
+        }
+        buttons.style.visibility = 'visible';
+    } 
+    $scope.AddListItem = function (){
+        var newListItem = angular.copy(basicListItem);
+        //add to top of list
+        $scope.finalList.splice(0,0, newListItem);
+    }
+    $scope.Reset = function(){
+        $scope.initialEntry = [];
+        $scope.finalList = [];
+        $scope.Init();
+    }
+    //scan and clean the input, insert into final array and create the table on screen
+    $scope.CheckAndInsertItemFromBulkListEntry = function(listItem, index){
+        
+        //check item before inserting into final array?
+        if(listItem == null || listItem == ""){
+            return;
+        }
+        //trim and insert into final array
+        listItem = listItem.trim();
+        var newItem = angular.copy(basicListItem);
+        newItem.name = listItem;
+        newItem.listIndex = index;
+        $scope.finalList.push(newItem);   
+    }
+    $scope.Init();
+}]);
---
+++ js/lbRow.js
@@ -0,0 +1,52 @@
+app.directive('lbRow', function(){
+    return {
+        restrict:'E',
+        scope: {
+            item: '=info',
+            items: '=infolist'
+        },
+        replace:true,
+        templateUrl:'listbuilder/js/lbRow.html',
+        link: function(scope, element, attrs){
+            scope.RemoveItem = function(item){
+                var itemIndex = scope.items.indexOf(item);
+                scope.items.splice(itemIndex, 1);
+            }
+            scope.CheckOffItem = function(item){
+                var currentIndex = scope.items.indexOf(item);
+                //store checked off item, then remove it and place it at the end
+                var checkedItem = scope.items[currentIndex];
+                
+                //if item currently checked, uncheck and move to top of checked off items
+                if(checkedItem.completed){
+                    scope.UncheckItem(checkedItem);
+                }else{
+                    scope.items.splice(currentIndex,1);
+                    scope.items.push(checkedItem);
+                }
+                //flip completed property
+                checkedItem.completed = !checkedItem.completed;
+            }
+            scope.UncheckItem = function(item){
+                //default the top checked index to the last element in case there is only one checked item
+                var topCheckedIndex = scope.items.length - 1;
+                var currentItem;
+
+                //find top completed item, and place newly unchecked item above that
+                for(var i = 0; i < scope.items.length - 1; i++){
+                    currentItem = scope.items[i];
+                    if(currentItem.completed){
+                        topCheckedIndex = scope.items.indexOf(currentItem);
+                        break;
+                    }
+                }
+                
+                //remove item being unchecked
+                scope.items.splice(scope.items.indexOf(item), 1);
+                //place in correct spot and change checked property
+                scope.items.splice(topCheckedIndex,0,item);
+                
+            }
+        }
+    }; 
+});
